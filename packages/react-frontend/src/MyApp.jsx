a104033813dcc9380654c181255ace4a87c47cfe
Sara V <lovechameleon@gmail.com>
2024-04-05 22:31:19 -0700

    Second commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,10 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    }, 
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -32,9 +16,10 @@ function MyApp() {
   return (
     <div className="container">
       <Table 
-      characterData={characters} 
-      removeCharacter={removeOneCharacter}
+        characterData={characters} 
+        removeCharacter={removeOneCharacter}
       />
+      <Form />
     </div>
   );
 }
