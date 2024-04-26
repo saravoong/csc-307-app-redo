3f2e833723ea08efa923d0390bcc4d61ab47db05
Sara V <lovechameleon@gmail.com>
2024-04-25 21:59:28 -0700

    For assignment #4, to the backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,14 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import {
+  getUsers,
+  addUser,
+  findUserById,
+  findUserByName,
+  findUserByJob,
+  findUserByNameAndJob,
+  deleteUserById } from "./user-services.js";
 
 const app = express();
 const port = 8000;
@@ -8,123 +16,66 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-
-  if (name !== undefined && job !== undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
+  getUsers(name, job)
+    .then((result) => {
+      if (result) {
+        res.send({ users_list: result });
+      } else {
+        res.status(404).send("Resource not found.");
+      }
+    })
 });
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  const id = req.params.id;
+  findUserById(id)
+    .then((result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(404).send("Resource not found.");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  const newUser = addUser(userToAdd);
-  res.status(201).json(newUser); 
+app.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  deleteUserById(userId)
+    .then((result) => {
+      if (result) {
+        res.status(204).send("User deleted successfully");
+      } else {
+        res.status(404).send("User not found");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    });
 });
-
-app.delete("/users/:userId", (req, res) => {
-  const userId = req.params.userId;
-  const userDeleted = deleteUser(userId);
-  if (userDeleted) {
-    res.send("User deleted successfully");
-  } else {
-    res.status(404).send("User not found");
-  }
-});
-
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    },
-    {
-      "id": "qwe123",
-      "job": "Zookeeper",
-      "name": "Cindy"
-    }
-  ]
-};
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
 });
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const addUser = (user) => {
-  const randomId = idGenerator();
-  user.id = randomId;
-  users["users_list"].push(user);
-  return user;
-};
-
-const deleteUser = (userId) => {
-  const index = users["users_list"].findIndex(user => user.id === userId);
-  if (index !== -1) {
-    users["users_list"].splice(index, 1);
-    return true; // Indicate successful deletion
-  }
-  return false; // Indicate user not found
-};
-
-const idGenerator = () => {
-  return Math.random().toString(36).substr(2, 6);
-};
-
 
